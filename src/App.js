0ee9a544d56a230981ac4bda1909a78397733085
JS <test@test.com>
2020-11-13 11:48:25 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  Hello Jeremy ee
+	  Hello - how are you?
         </p>
         <a
           className="App-link"
